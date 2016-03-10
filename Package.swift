d45e2920735882f3ecf49c1a670271f9ede8ffbf
Rachel Brindle <you@subluminal.net>
2016-03-09 19:16:08 -0800

    Split off Firmata.Swift into it's own package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Phobos",
     dependencies: [
-        .Package(url: "https://github.com/younata/CFirmata.git", majorVersion: 0),
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0),
         .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0),
 //        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 1),
